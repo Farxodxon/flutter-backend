8dca4b35c92f8f9ad709fece998573607c1688ec
Farxod <farxod@MacBook-Pro-Farxod.local>
2026-06-05 18:01:23 +0500

    CORS middleware qo'shildi

---
+++ routes/_middleware.dart
@@ -0,0 +1,28 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Handler middleware(Handler handler) {
+  return (context) async {
+    // OPTIONS so'rovlarga javob
+    if (context.request.method == HttpMethod.options) {
+      return Response(
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+      );
+    }
+
+    final response = await handler(context);
+
+    return response.copyWith(
+      headers: {
+        ...response.headers,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    );
+  };
+}
